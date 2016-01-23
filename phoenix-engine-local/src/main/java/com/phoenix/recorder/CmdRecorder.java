31ebf9173c5280a286a9ade54220a33b87de608e
Nigel Schuster <nigel.schusters@googlemail.com>
2016-01-23 11:56:26 -0600

    Added initial pom

---
+++ phoenix-engine-local/src/main/java/com/phoenix/recorder/CmdRecorder.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2016 Nigel Schuster.
+ */
+
+
+package com.phoenix.recorder;
+
+import com.phoenix.Runner;
+import com.phoenix.config.CmdArguments;
+
+/**
+ * @author nschuste
+ * @version 1.0.0
+ * @since Jan 23, 2016
+ */
+public class CmdRecorder implements Runner {
+  /**
+   * {@inheritDoc}
+   * 
+   * @author nschuste
+   * @version 1.0.0
+   * @see com.phoenix.Runner#executeArgs(com.phoenix.config.CmdArguments)
+   * @since Jan 23, 2016
+   */
+  @Override
+  public void executeArgs(final CmdArguments args) {}
+}
