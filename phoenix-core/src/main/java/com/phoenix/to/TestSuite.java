f24db65e8f354625d715518b60a5033a4ff7f39b
Nigel Schuster <nigel.schusters@googlemail.com>
2016-02-26 19:12:14 -0600

    Introduce TestSuite entity

---
+++ phoenix-core/src/main/java/com/phoenix/to/TestSuite.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2016 Nigel Schuster.
+ */
+
+
+package com.phoenix.to;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * @author nschuste
+ * @version 1.0.0
+ * @since Feb 26, 2016
+ */
+@Data
+@Accessors(chain = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestSuite {
+  @Id
+  private String id;
+  // https://docs.mongodb.org/manual/reference/database-references/
+  // Unless you have a compelling reason to use DBRefs, use manual references instead.
+  private Collection<String> testcaseids;
+}
